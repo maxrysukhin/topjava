329d96ca0c7ad3bfebb527e395184da9284499ab
Max Rysukhyn <m.mowgly@gmail.com>
2017-07-19 17:15:45 +0300

    added scripts for creating and populating meals table

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -11,3 +12,16 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, description, calories, datetime) VALUES
+  (100000, 'Завтрак', 500, '2016-01-02 10:00:00');
+INSERT INTO meals (user_id, description, calories, datetime) VALUES
+  (100000, 'Обед', 1000, '2016-01-02 13:00:00');
+INSERT INTO meals (user_id, description, calories, datetime) VALUES
+  (100000, 'Ужин', 500, '2016-01-02 20:00:00');
+INSERT INTO meals (user_id, description, calories, datetime) VALUES
+  (100001, 'Завтрак', 500, '2016-01-03 10:00:00');
+INSERT INTO meals (user_id, description, calories, datetime) VALUES
+  (100001, 'Обед', 1000, '2016-01-03 13:00:00');
+INSERT INTO meals (user_id, description, calories, datetime) VALUES
+  (100001, 'Ужин', 510, '2016-01-03 20:00:00');
