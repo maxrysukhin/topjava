ab62ae69a3551f5ecd4287fb2160f3f24995e725
Max Rysukhyn <m.mowgly@gmail.com>
2017-07-28 18:05:52 +0300

    Added RootControllerTest.testMeals & ResourceControllerTest

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.Test;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Created by max on 1/31/17.
+ */
+public class ResourceControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testCss() throws Exception{
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/css;"));
+
+    }
+}
